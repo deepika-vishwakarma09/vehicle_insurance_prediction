72f21a04bb5cf256ea491b7d19a7563dec8e0d2c
deepika-vishwakarma09 <vishwakarmadipu1975@gmail.com>
2025-04-18 01:07:28 +0530

    project structure ready,venv set

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="deepika-vishwakarma09",
+    author_email="vishwakarmadipu1975@gmail.com",
+    packages=find_packages()
+)
